054554b40c692e699bbdc086fbefd85a4cd7cac4
clarafp8 <frigolet.paiva.clara@iescamas.es>
2026-02-10 21:21:59 +0100

    Modificado la interfaz de Pelicula para poder utilizar un buscador

--- movie/src/main/java/com/movie/movie/repositorio/PeliculaRepositorio.java
+++ movie/src/main/java/com/movie/movie/repositorio/PeliculaRepositorio.java
@@ -1,10 +1,19 @@
 package com.movie.movie.repositorio;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.movie.movie.entidad.Pelicula;
 
 public interface PeliculaRepositorio extends JpaRepository<Pelicula, Long>{
     void deleteByDirectorId(Long directorId);
+    
+ // Método para encontrar cursos por su título
+    List<Pelicula> findByTitulo(String titulo);
+    
+    @Query(value = "SELECT * FROM pelicula WHERE titulo LIKE %?1%", nativeQuery = true)
+    List<Pelicula> buscarPorTituloParcialSQL(String titulo);
 
 }
